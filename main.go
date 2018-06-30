3129c2b55753e78955e7b39db670ce6069b14786
16861 <benefacta@gmail.com>
2018-06-30 17:31:16 +0300

    Add commets to structures Perso and Address

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//Person stores id, first, lastname and address of person
 type Person struct {
 	ID        string   `json:"id,omitempty"`
 	Firstname string   `json:"firstname,omitempty"`
 	Lastname  string   `json:"lastname,omitempty"`
 	Address   *Address `json:"address,omitempty"`
 }
+
+// Address stores cite and state
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
